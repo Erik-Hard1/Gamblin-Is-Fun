5343013356040423d235e003044c4e50ef59f6a3
Erik-Hard1 <13EH1104@amal.se>
2024-09-17 10:33:58 +0200

    Add project files.

---
+++ Gamblin' Is Fun/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.WriteLine("Welcome To My Gamling Dungeon. This Will Be Incredible.");
+
+Console.WriteLine("                                   88                                       ");
+Console.WriteLine("                                   88              ,d      ,d               ");
+Console.WriteLine("                                   88              88      88               ");
+Console.WriteLine("8b,dPPYba,  ,adPPYba,  88       88 88  ,adPPYba, MM88MMM MM88MMM ,adPPYba,  ");
+Console.WriteLine("88P'   \"Y8 a8\"     \"8a 88       88 88 a8P_____88   88      88   a8P_____88");
+Console.WriteLine("88         8b       d8 88       88 88 8PP\"\"\"\"\"\"\"   88      88   8PP\"\"\"\"\"\"\"  ");
+Console.WriteLine("88         "8a,   ,a8" "8a,   ,a88 88 "8b,   , aa   88, 88, "8b,   ,aa  ");
+bool betting = true;
+int bet;
+
+
+
+
+
+
+
+
+
